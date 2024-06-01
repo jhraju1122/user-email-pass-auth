32b9f4d5c19bda220aec35af53efffcb26896093
jhraju <jhraju890@gmail.com>
2024-06-01 23:15:13 +0600

    added register form

--- src/components/register/Register.jsx
+++ src/components/register/Register.jsx
@@ -2,8 +2,15 @@ import React from 'react';
 
 const Register = () => {
     return (
-        <div>
-            <h2 className="text-3xl text-center">please register</h2>
+        <div className='mx-auto border w-2/4'>
+            <h2 className="text-3xl text-center mt-5">please register</h2>
+            <form className='text-center'>
+                <input className='mb-4 mt-2 w-2/4' type="email" placeholder='type your email' name='email' id='' />
+                <br />
+                <input className='w-2/4' type="password" placeholder='type your password' name='password' id='' />
+                <br />
+                <input className='btn btn-secondary w-2/4 mt-4 mb-4 ' type="submit" value="Register" />
+            </form>
         </div>
     );
 };
